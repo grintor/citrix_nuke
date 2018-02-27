4869dd97ae8b87d9901707fdbabb8270317a7cd6
Chris Wheeler <grintor@gmail.com>
2018-02-27 11:34:57 -0500

    Update citrix_nuke.cmd

--- citrix_nuke.cmd
+++ citrix_nuke.cmd
@@ -1,10 +1,53 @@
+::------------------------------------------------------
+:: regular uninstall
+::------------------------------------------------------
+
+for /f "tokens=2*" %%a in ('reg query HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\CitrixOnlinePluginPackWeb /v UninstallString 2^>nul') do (
+   for /f "tokens=1 delims=/" %%c in ("%%b") do start /wait "" "%%c" /uninstall /cleanup /silent /noreboot
+)
+
+for /f "tokens=2*" %%a in ('reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\CitrixOnlinePluginPackWeb /v UninstallString 2^>nul') do (
+   for /f "tokens=1 delims=/" %%c in ("%%b") do start /wait "" "%%c" /uninstall /cleanup /silent /noreboot
+)
+
+::------------------------------------------------------
+:: official cleanup utility
+::------------------------------------------------------
+
+ReceiverCleanupUtility.exe  /silent /noreboot
+
+::------------------------------------------------------
+:: delete registration by official installed MSI CLSID's
+::------------------------------------------------------
+
+for /f %%n in ('wmic product where "name like 'Citrix Authentication Manager' or name like 'Citrix Receiver Inside' or name like 'Citrix Receiver(Aero)' or name like 'Citrix Receiver(DV)' or name like 'Citrix Receiver (HDX Flash Redirection)' or name like 'Citrix Receiver(USB)' or name like 'Online Plug-in' or name like 'Citrix Web Helper'" get identifyingnumber ^| find "}"') do call :del_by_CLSID %%n
+
+::------------------------------------------------------
+:: go crazy and start deleting anything that looks like citrix from the registry
+:: https://support.citrix.com/article/CTX212773
+::------------------------------------------------------
+
+call :del_product_reg Online Plug-in
+call :del_product_reg Citrix Authentication Manager
+call :del_product_reg Citrix Receiver Inside
+call :del_product_reg Citrix Receiver(Aero)
+call :del_product_reg Citrix Receiver(DV)
+call :del_product_reg Citrix Receiver (HDX Flash Redirection)
+call :del_product_reg Citrix Receiver(USB)
+call :del_product_reg Citrix Web Helper
+
+::-----------------------------------------------------
+:: go even more crazy
+:: https://support.citrix.com/article/CTX325140
+::-----------------------------------------------------
+
 for /f %%n in ('reg query hkcr ^| find "\Citrix.ICAClient"') do reg delete %%n /f
 for /f %%n in ('reg query hkcr ^| find "\Citrix.ICAClientProp"') do reg delete %%n /f
 reg delete hkcr\ica /f
 for /f %%n in ('reg query hkcr\CLSID ^| find "{238F"') do reg delete %%n /f
 reg delete hkcr\Installer\UpgradeCodes\9B123F490B54521479D0EDD389BCACC1 /f
 reg delete hkcr\Installer\UpgradeCodes\9B123F490B54521479D0EDD389BCACC1 /f
-reg delete hkcr\Mime\Database\Content Type\application/x-ica /f
+reg delete "hkcr\Mime\Database\Content Type\application/x-ica" /f
 reg delete hkcr\Wfica /f
 reg delete hkcr\WinFrameICA /f
 reg delete "hkcr\ICA Client" /f
@@ -62,16 +105,33 @@ reg delete "hklm\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall
 reg delete "hklm\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\CitrixOnlinePluginPackWeb" /f
 
 reg delete "hklm\Software\Policies\Citrix\ICA Client" /f
-reg delete "hkcu\\Software\Policies\Citrix\ICA Client" /f
+reg delete "hkcu\Software\Policies\Citrix\ICA Client" /f
 reg delete "hklm\Software\Wow6432Node\Policies\Citrix\ICA Client" /f
 
-for /f %%n in ('wmic product where "name like 'Citrix Authentication Manager' or name like 'Citrix Receiver Inside' or name like 'Citrix Receiver(Aero)' or name like 'Citrix Receiver(DV)' or name like 'Citrix Receiver (HDX Flash Redirection)' or name like 'Citrix Receiver(USB)' or name like 'Citrix Web Helper'" get identifyingnumber ^| find "}"') do call :del_by_CLSID %%n
 
+::-------------------------------------------------
+goto :EOF
+:del_product_reg
+for /f %%a in ('reg query hkcr\installer\products') do (
+   for /f %%h in ('reg query %%a /v ProductName 2^>nul ^| find "%*"') do (
+      for /f "tokens=4 delims=\" %%q in ('echo %%a') do (
+         reg delete HKEY_CLASSES_ROOT\Installer\Products\%%q /f
+         reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\%%q /f
+      )
+   )
+   
+)
+goto :EOF
+
+
+::-------------------------------------------------
 goto :EOF
 :del_by_CLSID
 set CLSID=%1
 set CLSID=%CLSID:}=%
 set CLSID=%CLSID:{=%
 set CLSID=%CLSID:-=%
+:: https://support.citrix.com/article/CTX212773
 reg delete HKEY_CLASSES_ROOT\Installer\Products\%CLSID% /f
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Products\%CLSID% /f
 goto :EOF
